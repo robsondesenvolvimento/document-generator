c0604bbe72aa306de49fb4212bb417e01f7c801d
Robson Alves <robsondesenvolvimento@gmail.com>
2021-07-20 00:18:40 -0300

    fix resilient axios

--- src/apicnh.ts
+++ src/apicnh.ts
@@ -1,4 +1,5 @@
-import axios from "axios";
+import * as rax from 'retry-axios';
+import axios, { AxiosInstance } from "axios";
 
 class ApiDocumentGeneratorCnh {
     private urlApi:string;
@@ -8,17 +9,59 @@ class ApiDocumentGeneratorCnh {
     }
 
     async cnhCreate() {
-        return axios.get<string>(`${this.urlApi}/cnh/create`)
+        let myAxiosInstance:AxiosInstance;
+        myAxiosInstance = axios.create();
+
+        myAxiosInstance.defaults.raxConfig = {
+            instance: myAxiosInstance,
+            retry: 3,
+            noResponseRetries: 2,
+            retryDelay: 2000,
+            httpMethodsToRetry: ['GET'],
+            //statusCodesToRetry: [[100, 199], [429, 429], [500, 599]],
+            backoffType: 'exponential'
+        };
+
+        const interceptorId = rax.attach(myAxiosInstance);
+        return await myAxiosInstance.get<string>(`${this.urlApi}/cnh/create`)
         .then(response => response.data);
     }
 
     async cnhCreateList() {
-        return await axios.get<string[]>(`${this.urlApi}/cnh/createlist`)
+        let myAxiosInstance:AxiosInstance;
+        myAxiosInstance = axios.create();
+
+        myAxiosInstance.defaults.raxConfig = {
+            instance: myAxiosInstance,
+            retry: 3,
+            noResponseRetries: 2,
+            retryDelay: 2000,
+            httpMethodsToRetry: ['GET'],
+            //statusCodesToRetry: [[100, 199], [429, 429], [500, 599]],
+            backoffType: 'exponential'
+        };
+
+        const interceptorId = rax.attach(myAxiosInstance);
+        return await myAxiosInstance.get<string[]>(`${this.urlApi}/cnh/createlist`)
         .then(response => response.data);
     }
 
     async cnhIsValid(cnh: string) {
-        return await axios.get<string[]>(`${this.urlApi}/cnh/isvalid/${cnh}`)
+        let myAxiosInstance:AxiosInstance;
+        myAxiosInstance = axios.create();
+
+        myAxiosInstance.defaults.raxConfig = {
+            instance: myAxiosInstance,
+            retry: 3,
+            noResponseRetries: 2,
+            retryDelay: 2000,
+            httpMethodsToRetry: ['GET'],
+            //statusCodesToRetry: [[100, 199], [429, 429], [500, 599]],
+            backoffType: 'exponential'
+        };
+
+        const interceptorId = rax.attach(myAxiosInstance);
+        return await myAxiosInstance.get<string[]>(`${this.urlApi}/cnh/isvalid/${cnh}`)
         .then(response => response.data);
     }
 }
--- src/apicpf.ts
+++ src/apicpf.ts
@@ -1,24 +1,68 @@
-import axios from "axios";
+import * as rax from 'retry-axios';
+import axios, { AxiosInstance } from "axios";
 
 class ApiDocumentGeneratorCpf {
     private urlApi:string;
+    
 
     constructor(){
-        this.urlApi = "https://robsonalves-net-br-document-generator-srvapp.azurewebsites.net/api/";
+        this.urlApi = "https://robsonalves-net-br-document-generator-srvapp.azurewebsites.net/api/";        
     }
 
-    async cpfCreate() {
-        return axios.get<string>(`${this.urlApi}/cpf/create`)
+    async cpfCreate() {    
+        let myAxiosInstance:AxiosInstance;
+        myAxiosInstance = axios.create();
+
+        myAxiosInstance.defaults.raxConfig = {
+            instance: myAxiosInstance,
+            retry: 3,
+            noResponseRetries: 2,
+            retryDelay: 2000,
+            httpMethodsToRetry: ['GET'],
+            //statusCodesToRetry: [[100, 199], [429, 429], [500, 599]],
+            backoffType: 'exponential'
+        };
+
+        const interceptorId = rax.attach(myAxiosInstance);
+        return await myAxiosInstance.get<string>(`${this.urlApi}/cpf/create`)
         .then(response => response.data);
     }
 
     async cpfCreateList() {
-        return await axios.get<string[]>(`${this.urlApi}/cpf/createlist`)
+        let myAxiosInstance:AxiosInstance;
+        myAxiosInstance = axios.create();
+
+        myAxiosInstance.defaults.raxConfig = {
+            instance: myAxiosInstance,
+            retry: 3,
+            noResponseRetries: 2,
+            retryDelay: 2000,
+            httpMethodsToRetry: ['GET'],
+            //statusCodesToRetry: [[100, 199], [429, 429], [500, 599]],
+            backoffType: 'exponential'
+        };
+
+        const interceptorId = rax.attach(myAxiosInstance);
+        return await myAxiosInstance.get<string[]>(`${this.urlApi}/cpf/createlist`)
         .then(response => response.data);
     }
 
     async cpfIsValid(cpf: string) {
-        return await axios.get<string[]>(`${this.urlApi}/cpf/isvalid/${cpf}`)
+        let myAxiosInstance:AxiosInstance;
+        myAxiosInstance = axios.create();
+
+        myAxiosInstance.defaults.raxConfig = {
+            instance: myAxiosInstance,
+            retry: 3,
+            noResponseRetries: 2,
+            retryDelay: 2000,
+            httpMethodsToRetry: ['GET'],
+            //statusCodesToRetry: [[100, 199], [429, 429], [500, 599]],
+            backoffType: 'exponential'
+        };
+
+        const interceptorId = rax.attach(myAxiosInstance);
+        return await myAxiosInstance.get<string[]>(`${this.urlApi}/cpf/isvalid/${cpf}`)
         .then(response => response.data);
     }
 }
